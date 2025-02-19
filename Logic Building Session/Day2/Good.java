80bfa892aa3151d2633e9d89a0dc7d798b4c3aa8
ChaudhariIsha <126152884+ChaudhariIsha@users.noreply.github.com>
2025-02-19 12:38:00 +0530

    Add files via upload

---
+++ Logic Building Session/Day2/Good.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Good{
+public static void main(String args[]){
+
+     Scanner scn = new Scanner(System.in);
+     double t;
+
+   System.out.println("Enter the time");
+     t = scn.nextDouble();
+
+     if(t>4 && t<=12){
+          System.out.println("Good Morning");}
+
+
+
+
+
+}}
